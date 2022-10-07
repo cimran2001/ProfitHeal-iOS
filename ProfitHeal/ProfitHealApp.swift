1694140a3090d4894ac3321907129e1f23f5370b
Imran Jabrayilov <c.imran2001@yandex.com>
2022-10-08 00:08:54 +0400

    Initial Commit

---
+++ ProfitHeal/ProfitHealApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfitHealApp.swift
+//  ProfitHeal
+//
+//  Created by Imran Jabrayilov on 04.10.22.
+//
+
+import SwiftUI
+
+@main
+struct ProfitHealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
