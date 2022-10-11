0fc62ebd471f98cdd665f52ecfcd8dda356ec070
Imran Jabrayilov <c.imran2001@yandex.com>
2022-10-11 21:43:56 +0400

    Home view

--- ProfitHeal/Views/Home/HomeView.swift
+++ ProfitHeal/Views/Home/HomeView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State public var sent: Bool = false
+    
+    private func getDate() -> Text {
+        let calendar = Calendar.current
+        let date = Date()
+        
+        let day = calendar.component(.day, from: date)
+        let monthName = calendar.monthSymbols[calendar.component(.month, from: date) - 1]
+        
+        return Text("Today, \(monthName) \(day)")
+    }
+    
     var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                HomeStatus()
-                RecentReportList()
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Hi, Imran!")
+                    .font(.title)
+                Spacer()
+                getDate()
+                    .font(.title)
             }
+            .padding()
+            
+            NavigationLink {
+                SymptomList(state: $sent)
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(.orange)
+                        .frame(height: 100)
+                    
+                    HStack {
+                        Spacer()
+                        Text("Send report")
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                }
+            }
+            
+            Spacer()
         }
+        .padding()
     }
 }
 
